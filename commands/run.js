154a6fee166b909316780ccba390692d5d11cc51
cliftonc <clifton.cunningham@gmail.com>
2014-08-15 08:31:47 +0100

    Initial pass at a run command

--- commands/run.js
+++ commands/run.js
@@ -1,3 +1,11 @@
+
+var _ = require('lodash');
+var async = require('async');
+var fs = require('fs');
+var http = require('http');
+var watch = require('watch');
+var exec = require('child_process').exec;
+
 module.exports = {
 	name:'run',
 	description:'Runs all of the microservices (or subset based on tag)',
@@ -7,10 +15,41 @@ module.exports = {
 
 function cmd(bosco, args) {
 	
-	// Loop through each MS
-	// Build list of Files + Tags from bosco.json files
-	// Using this list compile a set of compiled assets
-	// Push somewhere or serve
-	bosco.log("Run each mircoservice " + args);
+	var app = args.length > 0 ? args[0] : 'ALL';
+	var repos = bosco.config.get('github:repos');
+	var runnableServices = {};
 
+	var getRunnableServices = function() {
+		_.map(repos, function(repo) {
+			var repoPackage, basePath, repoPath = bosco.getRepoPath(repo), repoPackageJson = [repoPath,"package.json"].join("/");
+			if(bosco.exists(repoPackageJson)) {
+				repoPackage = require(repoPackageJson);
+				if(repoPackage.scripts && repoPackage.scripts.start) {
+					runService(repo, repoPackage.scripts.start, repoPath);
+				}				
+			}
+		});
+	}
+
+	var runService = function(repo, script, repoPath) {
+		bosco.log("Starting " + repo + " @ " + repoPath + " via " + script.blue);
+		run(repo, script, repoPath);
+	}
+
+	bosco.log("Run each mircoservice " + args);
+	getRunnableServices();
+
+}
+
+function run(repo, script, repoPath, next) {	
+	exec(script, {
+	  cwd: repoPath
+	}, function(err, stdout, stderr) {
+		if(err) {
+			bosco.error(stderr);
+		} else {
+			bosco.log(repo.purple + " " + stdout);
+		}
+		next(err);
+	});
 }
