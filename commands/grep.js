725383d538eea53f86fa5ecfa4fff618c0d94da0
Gabriel Cebrian <gabceb@gmail.com>
2014-09-15 09:12:59 -0700

    Added better example following other command examples Added validation for required pattern in command

--- commands/grep.js
+++ commands/grep.js
@@ -8,7 +8,7 @@ var red = '\u001b[41m \u001b[0m';
 module.exports = {
   name:'grep',
   description:'Finds a pattern across your repos excluding the node_modules folder',
-  example: 'bosco grep service',
+  example: 'bosco grep <patternToSearch>',
   cmd:cmd
 }
 
@@ -16,6 +16,8 @@ function cmd(bosco, args, next) {
   var repoPattern = bosco.options.repo;
   var repoRegex = new RegExp(repoPattern);
   var pattern = args.shift(0);
+
+  if (!pattern) return bosco.error('A search pattern is required for the grep command');
 
   var repos = bosco.config.get('github:repos');
   if(!repos) return bosco.error("You are repo-less :( You need to initialise bosco first, try 'bosco clone'.");
