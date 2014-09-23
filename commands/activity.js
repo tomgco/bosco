fe4ba648e1a6cbf4302d735b0a6d9542e007fecd
Gabriel Cebrian <gabceb@gmail.com>
2014-09-22 23:37:19 -0700

    Added a bit more sauce to the example

--- commands/activity.js
+++ commands/activity.js
@@ -15,7 +15,7 @@ var utils;
 module.exports = {
 	name:'summary',
 	description:'Outputs a summary of activity on the repos',
-	example:'bosco summary -r <repoPattern>',
+	example:'bosco summary -r <repoPattern> --since 2014-09-22T23:36:26-07:00',
 	cmd:cmd
 }
 
