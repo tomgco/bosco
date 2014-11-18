d657bc37eab9c73480a7deb7fa0a9d0c1ced16c4
Jeff Hanke <geophree@wikispaces.com>
2014-11-18 15:42:12 -0800

    DockerUtils: unroll and clarify if statement.

--- src/RunWrappers/DockerUtils.js
+++ src/RunWrappers/DockerUtils.js
@@ -102,19 +102,19 @@ function startContainer(bosco, docker, fqn, options, container, next) {
 
         function check() {
             checkRunning(checkPort, function(err, running) {
-                if(err || !running) {
-                    if (Date.now() > checkEnd) {
-                        process.stdout.write('\n');
-                        bosco.warn('Could not detect if ' + options.name.green + ' had started on port ' + ('' + checkPort).magenta + ' after ' + checkTimeout + 'ms');
-                        return next();
-                    } else {
-                        process.stdout.write('.');
-                        setTimeout(check, 500);
-                    }
-                } else {
+                if (!err && running) {
                     process.stdout.write('\n');
                     return next();
                 }
+
+                if (Date.now() > checkEnd) {
+                    process.stdout.write('\n');
+                    bosco.warn('Could not detect if ' + options.name.green + ' had started on port ' + ('' + checkPort).magenta + ' after ' + checkTimeout + 'ms');
+                    return next();
+                }
+
+                process.stdout.write('.');
+                setTimeout(check, 500);
             });
         }
         bosco.log('Waiting for ' + options.name.green + ' to respond on port ' + ('' + checkPort).magenta);
