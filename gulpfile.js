fbf2a49d1627689ad864af43c55caf2f9b062db6
Jeff Hanke <geophree@wikispaces.com>
2015-03-07 15:16:24 -0800

    Move "tests/" directory to standard "test/"

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,6 @@ gulp.task('default', function() {
 });
 
 gulp.task('test', function () {
-    return gulp.src('tests/*.test.js', {read: false})
+    return gulp.src('test/*.test.js', {read: false})
         .pipe(mocha({reporter: 'nyan'}));
 });
