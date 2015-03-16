4be6400f27b989a4426164177df1965fff550981
Josiah Truasheim <josiah@rapidapps.io>
2015-03-16 15:27:17 +0000

    Add `unlink` command
    Referses the effects of `bosco link` by unlinking all modules (or a subset using the -r syntax).
    
    Takes ages when you run it - be ready for a coffee break.

---
+++ commands/unlink.js
@@ -0,0 +1,185 @@
+var _ = require('lodash');
+var path = require('path');
+var fs = require('fs');
+var async = require('async');
+var util = require('util');
+var exec = require('child_process').exec;
+
+module.exports = {
+  name: 'unlink',
+  description: 'Automatically npm unlinks all projects in a workspace',
+  example: 'bosco unlink',
+  options: [
+    {
+      option: 'dry-run',
+      syntax: ['--dry-run', 'Print commands without unlinking']
+    }
+  ],
+  cmd: function (bosco, args, next) {
+    var repoPattern = bosco.options.repo;
+    var repoRegex = repoPattern && new RegExp(repoPattern);
+
+    var repos = bosco.getRepos();
+    var packageRepos = {};
+    var dependencyMap = {};
+    var dependentsMap = {};
+
+    var addDependency = function addDependency(dependency, dependent) {
+      if (!(dependency in dependencyMap)) {
+        dependencyMap[dependency] = [];
+      }
+
+      if (!(dependent in dependentsMap)) {
+        dependentsMap[dependent] = [];
+      }
+
+      dependencyMap[dependency].push(dependent);
+      dependentsMap[dependent].push(dependency);
+    }
+
+    var discardNonWorkspaceDependencies = function () {
+      var discard = _.difference(Object.keys(dependencyMap), Object.keys(packageRepos));
+
+      discard.forEach(function (packageName) {
+        delete dependencyMap[packageName];
+      });
+
+    };
+
+    // The dependency tree is gathered asyncronously
+    async.map(repos, function (repo, next) {
+      var repoPath = bosco.getRepoPath(repo);
+      var repoPackage = path.join(repoPath, 'package.json');
+
+      fs.readFile(path.join(repoPath, 'package.json'), function (err, data) {
+        if (err) {
+          bosco.log(util.format('skipping %s', repo));
+          return next();
+        }
+
+        var packageJson;
+        try {
+          packageJson = JSON.parse(data.toString());
+        } catch (err) {
+          bosco.log('failed to parse json from %s', repoPackage);
+          return next();
+        }
+
+        packageRepos[packageJson.name] = repo;
+
+        for (var dependency in packageJson.dependencies) {
+          addDependency(dependency, packageJson.name);
+        }
+
+        for (var dependency in packageJson.devDependencies) {
+          addDependency(dependency, packageJson.name);
+        }
+
+        return next();
+      });
+    }, function (err) {
+      if (err) {
+        return next(err);
+      }
+
+      var packageCount = Object.keys(packageRepos).length;
+      var packageDiff = packageCount;
+      var commands = [];
+
+      function isSelected(packageName) {
+        if (!(packageName in packageRepos)) {
+          return false;
+        }
+
+        var repo = packageRepos[packageName];
+
+        if (!repoRegex) {
+          return true;
+        }
+
+        return repoRegex.test(repo);
+      }
+
+      while (packageDiff !== 0 && packageCount > 0) {
+        bosco.log(util.format('%s packages remain', packageCount));
+
+        for (var packageName in packageRepos) {
+          var repo = packageRepos[packageName];
+          var repoPath = bosco.getRepoPath(repo);
+
+          if (packageName in dependencyMap && dependencyMap[packageName].length > 0) {
+            continue;
+          }
+
+          delete packageRepos[packageName];
+
+          if (isSelected(packageName)) {
+            commands.push([packageName, 'npm unlink', {cwd: repoPath}]);
+          }
+
+          if (packageName in dependentsMap) {
+            var isInstallRequired = false;
+
+            dependentsMap[packageName].forEach(function (dependency) {
+              var index = dependencyMap[dependency].indexOf(packageName);
+
+              if (index === -1) {
+                return;
+              }
+
+              dependencyMap[dependency].splice(index, 1);
+
+              if (isSelected(dependency)) {
+                isInstallRequired = true;
+                commands.push([packageName, util.format('npm unlink %s', dependency), {cwd: repoPath}]);
+              }
+            });
+
+            if (isInstallRequired) {
+              commands.push([packageName, 'npm install', {cwd: repoPath}]);
+            }
+          }
+        }
+
+        packageDiff = Object.keys(packageRepos).length - packageCount;
+        packageCount = Object.keys(packageRepos).length;
+      }
+
+      async.mapSeries(commands, function (cmd, next) {
+        var packageName = cmd[0];
+        var command = cmd[1];
+        var options = cmd[2];
+
+        bosco.log(util.format('%s %s', packageName.blue, command));
+
+        if (bosco.options.program.dryRun) {
+          return next();
+        }
+
+        return next();
+
+        exec(command, options, function (err, stdout, stderr) {
+          if (err) {
+            return next(err);
+          }
+
+          process.stdout.write(stdout);
+          process.stderr.write(stderr);
+
+          return next();
+        });
+      }, function (err) {
+        if (err) {
+          if (next) {
+            return next(err);
+          }
+
+          throw err;
+        }
+
+        bosco.log('Complete');
+        return next && next();
+      });
+    });
+  }
+}
