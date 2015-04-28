8e46309ebae2e07570599885fe671caf4db20ad8
cliftonc <clifton.cunningham@gmail.com>
2015-04-29 00:11:37 +0100

    Have bosco stop errored processes too - no more zombies

--- src/RunWrappers/Node.js
+++ src/RunWrappers/Node.js
@@ -24,19 +24,7 @@ Runner.prototype.disconnect = function(next) {
  */
 Runner.prototype.listRunning = function(detailed, next) {
 	pm2.list(function(err, list) {
-		var filteredList = _.filter(list, function(pm2Process){ return pm2Process.pm2_env.status === 'online' })
-
-		if(!detailed) return next(err, _.pluck(filteredList,'name'));
-		next(err, filteredList);
-	});
-}
-
-/**
- * List services that have been created but are not running
- */
-Runner.prototype.listNotRunning = function(detailed, next) {
-	pm2.list(function(err, list) {
-		var filteredList = _.filter(list, function(pm2Process){ return pm2Process.pm2_env.status !== 'online' })
+		var filteredList = _.filter(list, function(pm2Process){ return pm2Process.pm2_env.status === 'online' || pm2Process.pm2_env.status === 'errored' })
 
 		if(!detailed) return next(err, _.pluck(filteredList,'name'));
 		next(err, filteredList);
